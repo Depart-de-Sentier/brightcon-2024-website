37d448412de625c231820a28d5af3586d184f6b7
Tomás NAVARRETE GUTIÉRREZ <tomas.navarrete@list.lu>
2024-04-09 12:25:20 +0200

    update venue directions.

--- pages/venue.vue
+++ pages/venue.vue
@@ -14,8 +14,8 @@
               <p>ZAL TechCenter</p>
               <p>Hein-Saß-Weg 22</p>
               <p>21129 Hamburg, Germany</p>
-	      <p>+49 40 248 595 0</p>
-	      <p>info@zal.aero</p>
+              <p>+49 40 248 595 0</p>
+              <p>info@zal.aero</p>
             </div>
           </div>
         </div>
@@ -28,23 +28,43 @@
     The conference of Sept. 25-27 will be held on German Aerospace Center premises.
     -->
     <h3>How to reach Hamburg</h3>
-    <a href="https://www.hamburg-travel.com/">
-      From Hamburg Tourismus website:
-    </a>
     <ul>
       <li>
-        🚅 by Train. Hamburg's four long-distance train stations, the Central
-        Train Station, Hamburg Dammtor, Hamburg-Altona and Hamburg-Harburg, all
-        of which are integrated in Germany's ICE network, make Hamburg very easy
-        to reach.
+        🚅 by Train. <i>First</i>: don't get confused! Hamburg has three train
+        stations in the inner city - Main Station, Dammtor and Altona - going
+        from east to west. They are all very well connected to certain
+        city-parts and most of the long distance trains (ICE and IC) pass
+        through all of them. Although Main Station being the one where you can
+        catch basically all major lines of the public transport system.
       </li>
       <li>
-        ✈ By Plane. The route network of the Helmut Schmidt Airport is
-        extensive. Numerous airlines fly to international and national
-        destinations. Within Germany there are direct flights to Munich,
-        Stuttgart, Cologne and many other cities.
+        ✈ By Plane. Hamburg Airport in Poppenbüttel is directly connected to
+        the city with the S-Bahn S1. The line spans from the Airport in the east
+        to the western part of Hamburg and passes Main Station as well as
+        Altona.
+      </li>
+      <li>
+        🚗 By Car. There are two highways surrounding Hamburg: the A7 builds the
+        south-north connection ➡️ Flensburg/Kiel and Denmark, whereas the A1
+        connects to the east - ➡️Berlin. In principal Hamburg is quite free of
+        parking fees, only in the inner city center you'll have to pay for
+        parking spots. Most hotels have parking lots though.
       </li>
     </ul>
+
+    <h3>How to reach ZAL Tech Center</h3>
+
+    Located at Hein-Saß-Weg 22 in Finkenwerder the ZAL Tech Center is a little
+    off the grid. Nevertheless it's well connected to Hamburg through ferry and
+    the big tunnle under the Elbe. Once landed in Finkenwerder it's a 10 minute
+    walk to the Center itself (alongside the canal, it's cute). By ferry: We
+    suggest Ferry 64, starting at S1, S3, U3 - Landungsbrücken, departing in a
+    15-minute frequent. It's a 30-minute ride on the Elbe, with stops at
+    Fischmarkt, Dockland and Övelgönne (better option for those coming from the
+    Altona station). By bus/car: With wheels it's the Bus 150 starting in Altona
+    going through the Elbtunnel. Caution: depending on the time, this option is
+    not suggested due to frequent traffic jams.
+
     <!-- 
     <h2>Accomodation</h2>
     <b>
