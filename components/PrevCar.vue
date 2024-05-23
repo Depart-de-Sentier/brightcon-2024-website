946e43fc3ab1527224c870255adbb66360fecc5c
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-05-23 11:12:12 +0200

    [WIP] add carousel compo

---
+++ components/PrevCar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+  <h2>Images from <a href="https://2023.brightcon.link">Brightcon 2023</a></h2>
+    <div class="row" >
+    <div class="col">
+  <carousel :items-to-show="1" :autoplay="4000">
+    <slide v-for="slide in previmgs" :key="slide">
+    <NuxtImg :src=slide />
+    </slide>
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+  </div>
+</div>
+</div>
+</template>
+<script setup lang="ts"> 
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+const previmgs = [
+   "/images/onlyinbc/1.jpeg",
+   "/images/onlyinbc/2.jpeg",
+   "/images/onlyinbc/3.jpeg",
+   "/images/onlyinbc/4.jpeg",
+   "/images/onlyinbc/5.jpeg",
+   "/images/onlyinbc/6.jpeg",
+   "/images/onlyinbc/7.jpeg",
+   "/images/onlyinbc/8.jpeg",
+   "/images/onlyinbc/9.jpeg",
+   "/images/onlyinbc/10.jpeg",
+   "/images/onlyinbc/11.jpeg",
+   "/images/onlyinbc/12.jpeg",
+   "/images/onlyinbc/13.jpeg",
+   "/images/onlyinbc/14.jpeg",
+   "/images/onlyinbc/15.jpeg",
+  ];
+</script>
+
+<style>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
