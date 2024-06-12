76a6edc9da338ce1ab34bf31cf804eb4a04d8def
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-06-12 19:56:39 +0200

    add og-image

--- app.vue
+++ app.vue
@@ -5,3 +5,15 @@
     </NuxtLayout>
   </div>
 </template>
+<script lang="ts" setup>
+useSeoMeta({
+title: 'Brightcon 2024',
+ogTitle:'Brightcon 2024',
+description: 
+'Brightcon is an annual conference where the community of the open source framework for LCA Brightway meets. It enables interactions between Brightway developers, maintainers, contributors, users and adopters.',
+ogDescription:
+'Brightcon is an annual conference where the community of the open source framework for LCA Brightway meets. It enables interactions between Brightway developers, maintainers, contributors, users and adopters.',
+ogImage: '/images/2024_banner_linkedin.jpg',
+twitterCard: 'summary_large_image',
+})
+</script>
